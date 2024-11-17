e5319d6a5c072f9df718c66361a9f91a55c572e0
kozzze <kozlov.ed.vs@mail.ru>
2024-11-17 23:08:27 +0300

    Formatted code wuth clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 // Main func
-int main(){
-    std::string name;
-    std::cout<<"Enter u name";
-    std::cin>>name;
-    std::cout<< name << "Hello world" << endl;
-    return 0;
+int main() {
+  std::string name;
+  std::cout << "Enter u name";
+  std::cin >> name;
+  std::cout << name << "Hello world" << endl;
+  return 0;
 }
